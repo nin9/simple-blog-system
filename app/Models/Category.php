fb73f6a43a120384d980d8f27818bd29da0bf099
Ahmed Ali <ahmed.alawadly@gmail.com>
2019-02-03 22:06:46 +0200

    fixed a typo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function category(){
+    public function posts(){
         return $this->hasMany(Post::class);
     }
 }
