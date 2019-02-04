ac022628d8ab21847dd063bd3f0c5514b07c6b28
Ahmed Ali <ahmed.alawadly@gmail.com>
2019-02-04 11:58:34 +0200

    added navbar and master blades

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="{{ route('PostsIndex') }}">Blogs</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('PostsIndex') }}">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        @if(count($categories)> 0)
+                        @foreach($categories as $cat)
+                          <a href="{{ route('CategoryPostsIndex', $cat->id) }}">{{$cat->name}}</a>
+                        @endforeach
+                      @endif
+                    {{-- <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a> --}}
+                </div>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link float-right" href="#">Admin</a>
+            </li>
+        <ul>
+    </div>
+  </nav>
